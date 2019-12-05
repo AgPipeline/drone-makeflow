d6f28ce2d2e48f05538aa95b1abbcdfc122b1c65
Chris Schnaufer <schnaufer@email.arizona.edu>
2019-12-05 12:11:44 -0700

    Adding plot clip

--- run_makeflow.sh
+++ run_makeflow.sh
@@ -2,4 +2,5 @@
 
 $HOME/cctools/bin/makeflow --jx odm_workflow.jx --jx-args env.json $@
 $HOME/cctools/bin/makeflow --jx soil_mask_workflow.jx --jx-args env.json $@
+$HOME/cctools/bin/makeflow --jx plot_clip_workflow.jx --jx-args env.json --jx-define "MF_BETYDB_URL"="`echo "\"\\\"\"${BETYDB_URL}\\\"\"\""`" --jx-define "MF_BETYDB_KEY"="`echo "\"\\\"\"${BETYDB_KEY}\\\"\"\""`" $@
 
