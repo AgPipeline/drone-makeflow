a894e03fcbf8e6da0ddcd0c606f3c241a4289422
Chris Schnaufer <schnaufer@email.arizona.edu>
2020-05-21 15:09:02 -0700

    Changed to use soilmask repo code

--- prep-canopy-cover.sh
+++ prep-canopy-cover.sh
@@ -6,7 +6,8 @@ echo "{\"CANOPYCOVER_FILE_LIST\": [" >> "/scif/data/soilmask/canopycover_filesli
 sep=""
 for entry in ${clips[@]}
 do
-  possible="${entry}/soilmask-output.tif"
+  possible="${entry}/odm_orthophoto_mask.tif"
+  echo "Checking possible ${possible}"
   if [ -f "${possible}" ]; then
     echo "${sep}{\"FILE\": \"${possible}\"," >> "/scif/data/soilmask/canopycover_fileslist.json"
     echo "\"DIR\": \"${entry}/\"}" >> "/scif/data/soilmask/canopycover_fileslist.json"
