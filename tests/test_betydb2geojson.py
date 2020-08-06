b6ceb1f20da0c5950b404622d5ad789b93a71370
Chris Schnaufer <schnaufer@email.arizona.edu>
2020-08-06 14:46:51 -0700

    Initial testing

---
+++ tests/test_betydb2geojson.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Purpose: Unit testing for betydb2geojson.py
+Author : Chris Schnaufer <schnaufer@arizona.edu
+Notes:
+    This file assumes it's in a subfolder off the main folder
+"""
+import os
+import re
+import pytest
+from subprocess import getstatusoutput
+
+# The name of the source file to test and it's path
+SOURCE_FILE = 'betydb2geojson.py'
+SOURCE_PATH = os.path.abspath(os.path.join('.', SOURCE_FILE))
+
+# BETYdb instance to hit up
+BETYDB_URL = 'https://terraref.ncsa.illinois.edu/bety'
+
+
+def test_exists():
+    """Asserts that the source file is available"""
+    assert os.path.isfile(SOURCE_PATH)
+
+
+def test_usage():
+    """
+    Program prints a "usage" statement when requested
+    """
+
+    for flag in ['-h', '--help']:
+        ret_val, out = getstatusoutput(f'{SOURCE_PATH} {flag}')
+        assert ret_val == 0
+        assert re.match('usage', out, re.IGNORECASE)
+
+
+def test_fail_betydb_url():
+    """Test that not specifying a BETYdb URL fails
+    """
+    import betydb2geojson as b2j
+    with pytest.raises(RuntimeError):
+        b2j.query_betydb_experiments()
+
+
+def test_betydb_url():
+    """Test getting BETYdb data
+    """
+    import betydb2geojson as b2j
+    ret_val = b2j.query_betydb_experiments(BETYDB_URL)
+    assert ret_val is not None
+    for key in ['metadata', 'data']:
+        assert key in ret_val
