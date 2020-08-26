a04e850a94980ed72b030c0b408e434c12a140b9
Chris Schnaufer <schnaufer@email.arizona.edu>
2020-08-26 11:14:48 -0700

    Updating for clean

--- generate_geojson.sh
+++ generate_geojson.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 FILE_PARAM="${1}"
-if [[ ! "${2}" == "" ]]; then
+if [[ "${2}" != "" ]]; then
   DESTINATION_FILE="${2}"
 else
   DESTINATION_FILE="/output/plots.json"
 fi
 
-if [[ ${FILE_PARAM} == http* ]]; then
-  scif run betydb2geojson --betydb_url "${FILE_PARAM}" "${DESTINATION_FILE}"
-elif [[ ${FILE_PARAM} == *.shp ]]; then
-  scif run shp2geojson "${SCIF_APPDATA_odm_workflow}/images/${FILE_PARAM}" "${DESTINATION_FILE}"
-elif [[ ${FILE_PARAM} == *.json ]]; then
-  cp "${SCIF_APPDATA_odm_workflow}/images/${FILE_PARAM}" "${DESTINATION_FILE}"
+if [[ "${3}" != *"--clean"* ]]; then
+  if [[ ${FILE_PARAM} == http* ]]; then
+    scif run betydb2geojson --betydb_url "${FILE_PARAM}" "${DESTINATION_FILE}"
+  elif [[ ${FILE_PARAM} == *.shp ]]; then
+    scif run shp2geojson "${FILE_PARAM}" "${DESTINATION_FILE}"
+  elif [[ ${FILE_PARAM} == *.json || ${FILE_PARAM} == *.geojson ]]; then
+    cp "${SCIF_APPDATA_odm_workflow}/images/${FILE_PARAM}" "${DESTINATION_FILE}"
+  else
+    echo "Unknown plot geometries file specified: \"${FILE_PARAM}\""
+    exit 1
+  fi
 else
-  echo "Unknown plot geometries file specified: \"${FILE_PARAM}\""
-  exit 1
+  rm "${DESTINATION_FILE}"
 fi
