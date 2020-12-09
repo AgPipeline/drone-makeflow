bfe3d97b973e4c0a32c89451a296e26d03193eb4
Chris Schnaufer <schnaufer@email.arizona.edu>
2020-12-09 13:57:14 -0700

    Changes based on CyVerse testing

--- cyverse_greenness-indices.sh
+++ cyverse_greenness-indices.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+WORKING_FOLDER=$(pwd)
+
 # Get the file JSON list
 if [[ "${1}" == "" ]]; then
   echo "The JSON file containing the file list was not specified for greenness indices"
@@ -21,3 +23,24 @@ echo "{" >"/scif/apps/src/jx-args.json"
 } >>"/scif/apps/src/jx-args.json"
 
 scif run greenness-indices
+
+# Copy the CSV files to the working folder as output
+FOUND_FILES=()
+while IFS= read -r -d '' ONE_FILE; do
+  case "${ONE_FILE: -4}" in
+    ".csv")
+      FOUND_FILES+=("${ONE_FILE}")
+      ;;
+  esac
+done < <(find "${WORKING_FOLDER}" -type f -print0)
+
+for i in "${FOUND_FILES[@]}"; do
+  FILE_NAME=$(basename "${i}")
+  FILE_PATH=$(dirname "${i}")
+  LAST_FOLDER_NAME=$(basename "${FILE_PATH}")
+  # Only copy files where the destination is not the same as the origin
+  if [[ "${i}" != "${WORKING_FOLDER}/${LAST_FOLDER_NAME}/${FILE_NAME}" ]]; then
+    mkdir -p "${WORKING_FOLDER}/${LAST_FOLDER_NAME}"
+    cp -f "${i}" "${WORKING_FOLDER}/${LAST_FOLDER_NAME}/${FILE_NAME}"
+  fi
+done
