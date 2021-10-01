0d2678ab3fa4d89ff433e222f4c14a7488a07929
Chris Schnaufer <schnaufer@email.arizona.edu>
2021-10-01 13:53:03 -0700

    Testing updates

--- plantit-workflow.sh
+++ plantit-workflow.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# INPUT=$PWD/test_data  WORKDIR=$PWD WORKFLOW_TO_RUN="Canopy Cover with normal soil masking" ./plantit-workflow.sh
+# cat /scif/apps/src/jx-args.json
+
 # Initialize variables
 IMAGE_FILE=
 PLOTS_FILE=
@@ -42,7 +45,7 @@ while IFS= read -r -d '' ONE_FILE; do
 done < <(find "${INPUT}" -maxdepth 1 -type f -print0)
 
 # Make sure we have mandatory files
-if [[ "{$IMAGE_FILE}" == "" ]]; then
+if [[ "${IMAGE_FILE}" == "" ]]; then
   echo "Unable to find source image file (*.tif or *.tiff)"
   exit 10
 fi
@@ -51,29 +54,34 @@ if [[ "${PLOTS_FILE}" == "" ]]; then
   exit 11
 fi
 
+echo "Processing with ${IMAGE_FILE} ${PLOTS_FILE}"
+echo "  Options: ${ALGO_OPTIONS}"
+
 # Write the argument JSON file for supported workflows (not all of them)
-echo "{" >"/scif/apps/src/jx-args.json"
-echo "\"SOILMASK_SOURCE_FILE\": \"/${INPUT}/${IMAGE_FILE}\"," >"/scif/apps/src/jx-args.json"
-echo "\"SOILMASK_MASK_FILE\": \"orthomosaicmask.tif\"," >"/scif/apps/src/jx-args.json"
-echo "\"SOILMASK_WORKING_FOLDER\": \"/${WORKDIR}\"," >"/scif/apps/src/jx-args.json"
-echo "\"SOILMASK_OPTIONS\": \"\"," >"/scif/apps/src/jx-args.json"
-echo "\"SOILMASK_RATIO_SOURCE_FILE\": \"/${INPUT}/${IMAGE_FILE}\"," >"/scif/apps/src/jx-args.json"
-echo "\"SOILMASK_RATIO_MASK_FILE\": \"orthomosaicmask.tif\"," >"/scif/apps/src/jx-args.json"
-echo "\"SOILMASK_RATIO_WORKING_FOLDER\": \"/${WORKDIR}\"," >"/scif/apps/src/jx-args.json"
-echo "\"SOILMASK_RATIO_OPTIONS\": \"--ratio 1.0\"," >"/scif/apps/src/jx-args.json"
-echo "\"PLOTCLIP_SOURCE_FILE\": \"/${WORKDIR}/orthomosaicmask.tif\"," >"/scif/apps/src/jx-args.json"
-echo "\"PLOTCLIP_PLOTGEOMETRY_FILE\": \"/${WORKDIR}/plots.geojson\"," >"/scif/apps/src/jx-args.json"
-echo "\"PLOTCLIP_WORKING_FOLDER\": \"/${WORKDIR}\"," >"/scif/apps/src/jx-args.json"
-echo "\"PLOTCLIP_OPTIONS\": \"\"," >"/scif/apps/src/jx-args.json"
-echo "\"FILES2JSON_SEARCH_NAME\": \"orthomosaicmask.tif\"," >"/scif/apps/src/jx-args.json"
-echo "\"FILES2JSON_SEARCH_FOLDER\": \"/${WORKDIR}\"," >"/scif/apps/src/jx-args.json"
-echo "\"FILES2JSON_JSON_FILE\": \"/${WORKDIR}/canopy_cover_files.json\"," >"/scif/apps/src/jx-args.json"
-echo "\"CANOPYCOVER_OPTIONS\": \"${OPTIONS}\"," >"/scif/apps/src/jx-args.json"
-echo "\"GREENNESS_INDICES_OPTIONS\": \"${OPTIONS}\"," >"/scif/apps/src/jx-args.json"
-echo "\"MERGECSV_SOURCE\": \"/${INPUT}\"," >"/scif/apps/src/jx-args.json"
-echo "\"MERGECSV_TARGET\": \"/${WORKDIR}\"," >"/scif/apps/src/jx-args.json"
-echo "\"MERGECSV_OPTIONS\": \"\"," >"/scif/apps/src/jx-args.json"
-echo "}" >"/scif/apps/src/jx-args.json"
+cat >"/scif/apps/src/jx-args.json" <<EOF
+{
+"SOILMASK_SOURCE_FILE": "${IMAGE_FILE}",
+"SOILMASK_MASK_FILE": "orthomosaicmask.tif",
+"SOILMASK_WORKING_FOLDER": "${WORKDIR}",
+"SOILMASK_OPTIONS": "",
+"SOILMASK_RATIO_SOURCE_FILE": "${IMAGE_FILE}",
+"SOILMASK_RATIO_MASK_FILE": "orthomosaicmask.tif",
+"SOILMASK_RATIO_WORKING_FOLDER": "${WORKDIR}",
+"SOILMASK_RATIO_OPTIONS": "--ratio 1.0",
+"PLOTCLIP_SOURCE_FILE": "${WORKDIR}/orthomosaicmask.tif",
+"PLOTCLIP_PLOTGEOMETRY_FILE": "${PLOTS_FILE}",
+"PLOTCLIP_WORKING_FOLDER": "${WORKDIR}",
+"PLOTCLIP_OPTIONS": "",
+"FILES2JSON_SEARCH_NAME": "orthomosaicmask.tif",
+"FILES2JSON_SEARCH_FOLDER": "${WORKDIR}",
+"FILES2JSON_JSON_FILE": "/scif/apps/src/canopy_cover_files.json",
+"CANOPYCOVER_OPTIONS": "${OPTIONS}",
+"GREENNESS_INDICES_OPTIONS": "${OPTIONS}",
+"MERGECSV_SOURCE": "${WORKDIR}",
+"MERGECSV_TARGET": "${WORKDIR}",
+"MERGECSV_OPTIONS": "",
+}
+EOF
 
 # Determine what workflow the caller wants to run, and run  that workflow
 if [[ "${WORKFLOW_TO_RUN}" == "Canopy Cover with normal soil masking" ]]; then
@@ -86,6 +94,9 @@ else
 fi
 
 echo "Running workflow steps: ${WORKFLOW[*]}"
-for (( IDX=0; IDX < ${#WORKFLOW[@]}; IDX++ )); do
+for ((IDX = 0; IDX < ${#WORKFLOW[@]}; IDX++)); do
   echo "Running app $IDX '${WORKFLOW[$IDX]}'"
+  scif run "${WORKFLOW[$IDX]}"
 done
+
+echo "Workflow completed"
